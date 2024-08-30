61ebf519030bdbd6faa09e925e6af24bfead8b5a
scriptKid <gopikrishnan0045@gmail.com>
2024-08-30 14:33:17 +0530

    add router

--- mediaplayer/src/App.jsx
+++ mediaplayer/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./assets/pages/home";
+import Navbar from "./assets/components/navbar";
+import Login from "./assets/pages/login";
+import Songs from "./assets/pages/songs";
+import Signup from "./assets/pages/signup";
 function App() {
   return (
     <>
-      <Home/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" component={<Home/>}/>
+          <Route path="/songs" component={<Songs/>}/>
+          <Route path="/login" component={<Login/>}/>
+          <Route path="/signup" component={<Signup/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
